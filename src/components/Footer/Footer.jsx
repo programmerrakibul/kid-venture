fe61b8855427c36692cf86eead501fa3f791cae6
Programmer Rakibul <rakibul00206@gmail.com>
2025-10-23 01:47:46 +0600

    footer modified

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router";
 import Container from "../Container/Container";
+import { SiFacebook, SiGithub, SiLinkedin } from "react-icons/si";
+import { IoLogoTwitter } from "react-icons/io";
 
 const Footer = () => {
   return (
@@ -12,12 +14,13 @@ const Footer = () => {
           >
             KidVenture
           </Link>
-          <p>
-            ACME Industries Ltd.
-            <br />
-            Providing reliable tech since 1992
+          <p className="max-w-sm opacity-80">
+            Your premier destination for magical toys and creative adventures,
+            offering carefully curated playthings that spark curiosity, foster
+            development.
           </p>
         </aside>
+
         <nav>
           <h6 className="footer-title">Services</h6>
           <a className="link link-hover">Branding</a>
@@ -25,18 +28,28 @@ const Footer = () => {
           <a className="link link-hover">Marketing</a>
           <a className="link link-hover">Advertisement</a>
         </nav>
-        <nav>
-          <h6 className="footer-title">Company</h6>
-          <a className="link link-hover">About us</a>
-          <a className="link link-hover">Contact</a>
-          <a className="link link-hover">Jobs</a>
-          <a className="link link-hover">Press kit</a>
-        </nav>
+
         <nav>
           <h6 className="footer-title">Legal</h6>
           <a className="link link-hover">Terms of use</a>
           <a className="link link-hover">Privacy policy</a>
           <a className="link link-hover">Cookie policy</a>
+        </nav>
+
+        <nav>
+          <h6 className="footer-title">Social Links</h6>
+          <a className="link link-hover">
+            <SiFacebook className="inline-block mr-1.5" /> Facebook
+          </a>
+          <a className="link link-hover">
+            <SiGithub className="inline-block mr-1.5" /> Github
+          </a>
+          <a className="link link-hover">
+            <IoLogoTwitter className="inline-block mr-1.5" /> Twitter
+          </a>
+          <a className="link link-hover">
+            <SiLinkedin className="inline-block mr-1.5" /> Linkedin
+          </a>
         </nav>
       </Container>
     </footer>
